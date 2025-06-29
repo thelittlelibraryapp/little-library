0f0f346cf147ad01fb36429e324cf0bd80c74725
Matt <m.dembling@gmail.com>
2025-06-28 22:41:39 -0400

    Fix Supabase build environment variables

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/supabase-js'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 }
 
 module.exports = nextConfig
